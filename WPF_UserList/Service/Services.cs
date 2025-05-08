95e055df80389ea58c24fd3a37e4f4ad3c727e70
rakesh2611-code <rakeshmahajan48@gmail.com>
2025-05-08 20:49:29 +0530

    1st

--- WPF_UserList/Service/Services.cs
+++ WPF_UserList/Service/Services.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using WPF_UserList.Model;
 
 namespace WPF_UserList.Service
@@ -12,13 +13,20 @@ namespace WPF_UserList.Service
     public class Services
     {
         private readonly HttpClient _httpClient = new HttpClient();
-
         public async Task<List<User>> GetUsersAsync()
         {
-            var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
-            response.EnsureSuccessStatusCode();
-            var json = await response.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<List<User>>(json);
+            try
+            {
+                var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<User>>(json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
         }
     }
 }
